0bfd43eea7f8beb7b7df3964a85feae7827c5b85
walterf <walter.fu.1064@gmail.com>
2026-04-12 20:31:16 -0700

    Fix imports

--- czt/__init__.py
+++ czt/__init__.py
@@ -1,2 +1,2 @@
-from czt.czt1d import ChirpZTransform1D
-from czt.czt2d import ChirpZTransform2D
+from .czt1d import ChirpZTransform1D
+from .czt2d import ChirpZTransform2D
--- zernike/__init__.py
+++ zernike/__init__.py
@@ -1,1 +1,1 @@
-from zernike.zernike_polynomials import generate_zernike_polynomial, get_noll_index
+from .zernike_polynomials import generate_zernike_polynomial, get_noll_index
